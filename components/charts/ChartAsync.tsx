46ac58a78c87ca66c3cf8ea0331e837956a40f24
Conor <conor@johngregg.org>
2022-12-18 10:31:04 -0600

    Use react-query

--- components/charts/ChartAsync.tsx
+++ components/charts/ChartAsync.tsx
@@ -1,34 +1,24 @@
-import {useState} from "react";
+import {useQuery} from "react-query";
 import {Chart, registerables} from 'chart.js';
 
 Chart.register(...registerables)
 
-const ChartAsync = (chartType: Function) => (chartOptions: {}, getChartData: Function) => {
+const ChartAsync = (chartType: Function) => (chartOptions: {}, getChartData: () => Promise<any>) => {
     const Chart = () => {
-        const [state, setState] = useState({
-            error: null as any,
-            isLoaded: false,
-            data: null as any
-        })
-
-        getChartData()
-            .then(
-                (data: {}) => setState({...state, data, isLoaded: true}),
-                (error: {}) => setState({...state, error, isLoaded: true}))
+        const { data, error, isError, isLoading } = useQuery('chartData', getChartData)
 
         return (
             <>
-                { state.error ? (
-                    <div>Error: {state.error.message}</div>
-                ) : state.isLoaded ? (
-                    <>
-                        <p>{ state.isLoaded ? "true" : "false" }</p>
-                        { chartType(chartOptions, state.data) }
-                    </>
-                ) : (
+                { isError ? (
+                    <div>Error</div>
+                ) : isLoading ? (
                     <div className="spinner-border" role="status">
                         <span className="sr-only">Loading...</span>
                     </div>
+                ) : (
+                    <>
+                        { chartType(chartOptions, data) }
+                    </>
                 )}
             </>
         )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/layout"
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
 
 const App = ({ Component, pageProps }: AppProps) => (
     <>
         <Head>
             <title>Flashback</title>
         </Head>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </QueryClientProvider>
+
     </>
 )
 
