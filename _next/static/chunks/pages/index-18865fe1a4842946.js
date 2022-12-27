18b8d1da638a8ede55f09309a7cfb23f149dc4d2
Conor <conor@johngregg.org>
2022-12-27 14:54:46 -0600

    Updates

---
+++ _next/static/chunks/pages/index-18865fe1a4842946.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{7314:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(6147)}])},6147:function(n,u,_){"use strict";_.r(u);var e=_(2322);let t=()=>(0,e.jsx)("h1",{children:"Home"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7314)}),_N_E=n.O()}]);
