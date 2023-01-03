86f9328cc6b12cfd4d5e5d87ff7c057f81f2c2b7
Conor <conor@johngregg.org>
2023-01-02 19:26:40 -0600

    load plays

--- packages/site/pages/api/loadPlays.ts
+++ packages/site/pages/api/loadPlays.ts
@@ -19,7 +19,7 @@ export default async (req, res) => {
   });
 
   // Get Plays
-  const plays = [];
+  let plays = [];
   const track_ids = new Set<string>();
 
   Object.getOwnPropertyNames(data["files"]).forEach((fileName) => {
@@ -43,29 +43,63 @@ export default async (req, res) => {
   }
 
   try {
-    await connection.execute("CREATE TABLE PLAY (" +
-      "TS TIMESTAMP," +
-      "USERNAME VARCHAR2(128)," +
-      "PLATFORM VARCHAR2(128)," +
-      "MS_PLAYED NUMBER(16, 0)," +
-      "CONN_COUNTRY VARCHAR2(16)," +
-      "IP_ADDR_DECRYPTED VARCHAR2(32)," +
-      "USER_AGENT_DECRYPTED VARCHAR2(64)," +
-      "MASTER_METADATA_TRACK_NAME VARCHAR2(256)," +
-      "MASTER_METADATA_ALBUM_ARTIST_NAME VARCHAR2(256)," +
-      "MASTER_METADATA_ALBUM_ALBUM_NAME VARCHAR2(256)," +
-      "SPOTIFY_TRACK_URI VARCHAR2(256)," +
-      "REASON_START VARCHAR2(128)," +
-      "REASON_END VARCHAR2(128)," +
-      "SHUFFLE NUMBER(8, 0)," +
-      "OFFLINE NUMBER(8, 0)," +
-      "OFFLINE_TIMESTAMP NUMBER(32, 0)," +
-      "INCOGNITO_MODE NUMBER(8, 0))")
-  } catch (err) {
-    console.error(err)
+    await connection.execute("SELECT * FROM PLAY FETCH FIRST 1 ROWS ONLY");
+
+  } catch {
+    console.log("PLAY table not found")
+    try {
+      await connection.execute("CREATE TABLE PLAY (TS TIMESTAMP,USERNAME VARCHAR2(128),PLATFORM VARCHAR2(128),MS_PLAYED NUMBER(16, 0),CONN_COUNTRY VARCHAR2(16),IP_ADDR_DECRYPTED VARCHAR2(64),USER_AGENT_DECRYPTED VARCHAR2(128),MASTER_METADATA_TRACK_NAME VARCHAR2(256),MASTER_METADATA_ALBUM_ARTIST_NAME VARCHAR2(256),MASTER_METADATA_ALBUM_ALBUM_NAME VARCHAR2(256),SPOTIFY_TRACK_URI VARCHAR2(256),REASON_START VARCHAR2(128),REASON_END VARCHAR2(128),SHUFFLE NUMBER(8, 0),OFFLINE_MODE NUMBER(8, 0),OFFLINE_TIMESTAMP NUMBER(32, 0),INCOGNITO_MODE NUMBER(8, 0))")
+
+    } catch (err) {
+      console.error(err)
+    }
   }
 
-  res.json({});
+  // Add entries to PLAY
+  try {
+    const sql = "INSERT INTO PLAY (TS, USERNAME, PLATFORM, MS_PLAYED, CONN_COUNTRY, IP_ADDR_DECRYPTED, USER_AGENT_DECRYPTED, MASTER_METADATA_TRACK_NAME, MASTER_METADATA_ALBUM_ARTIST_NAME, MASTER_METADATA_ALBUM_ALBUM_NAME, SPOTIFY_TRACK_URI, REASON_START, REASON_END, SHUFFLE, OFFLINE_MODE, OFFLINE_TIMESTAMP, INCOGNITO_MODE) VALUES (:ts, :username, :platform, :ms_played, :conn_country, :ip_addr_decrypted, :user_agent_decrypted, :master_metadata_track_name, :master_metadata_album_artist_name, :master_metadata_album_album_name, :spotify_track_uri, :reason_start, :reason_end, :shuffle, :offline_mode, :offline_timestamp, :incognito_mode)"
+
+    const options = {
+      autoCommit: true,
+      bindDefs: {
+        ts: { type: oracledb.STRING, maxSize: 128 },
+        username: { type: oracledb.STRING, maxSize: 128 },
+        platform: { type: oracledb.STRING, maxSize: 128 },
+        ms_played: { type: oracledb.NUMBER },
+        conn_country: { type: oracledb.STRING, maxSize: 16 },
+        ip_addr_decrypted: { type: oracledb.STRING, maxSize: 128 },
+        user_agent_decrypted: { type: oracledb.STRING, maxSize: 256 },
+        master_metadata_track_name: { type: oracledb.STRING, maxSize: 256 },
+        master_metadata_album_artist_name: { type: oracledb.STRING, maxSize: 256},
+        master_metadata_album_album_name: { type: oracledb.STRING, maxSize: 256 },
+        spotify_track_uri: { type: oracledb.STRING, maxSize: 256 },
+        reason_start: { type: oracledb.STRING, maxSize: 128 },
+        reason_end: { type: oracledb.STRING, maxSize: 128 },
+        shuffle: { type: oracledb.NUMBER },
+        offline_mode: { type: oracledb.NUMBER },
+        offline_timestamp: { type: oracledb.NUMBER },
+        incognito_mode: { type: oracledb.NUMBER },
+      }
+    }
+
+    plays.forEach(play => {
+      play["offline_mode"] = play["offline"] ? 1 : 0;
+      play["shuffle"] = play["shuffle"] ? 1 : 0;
+      play["incognito_mode"] = play["incognito_mode"] ? 1 : 0;
+      play["ts"] = `${play["ts"].substring(0, 10)} ${play["ts"].substring(11, 19)}`
+    });
+
+    await connection.execute("ALTER SESSION SET NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS'");
+
+    const result = await connection.executeMany(sql, plays, options);
+
+    console.log(result.rowsAffected);
+
+    res.json({ insertedCount: result.rowsAffected });
+  } catch (err) {
+    console.error(err);
+    res.error(err);
+  }
   // if (req.method === "POST") {
   //   let connection: Connection;
   //
