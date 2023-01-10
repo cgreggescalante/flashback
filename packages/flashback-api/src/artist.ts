1d948fe7b79066699aac9d727d74763c783a3c96
Conor <conor@johngregg.org>
2023-01-09 18:58:49 -0600

    Added get to ArtistAPI

--- packages/flashback-api/src/artist.ts
+++ packages/flashback-api/src/artist.ts
@@ -1,12 +1,34 @@
 import { dateToTimestamp, postStatement } from "./utils";
 
+interface getByPlayTimeOptions {
+  offset?: number,
+  limit?: number,
+  rangeStart?: Date,
+  rangeEnd?: Date
+}
+
 export class ArtistAPI {
+  static get = async (
+    artistId: string
+  ): Promise<any[]> => {
+    return await postStatement({
+      statementText: "SELECT * FROM ARTIST WHERE ID = :id",
+      binds: [
+        {
+          name: "id",
+          data_type: "VARCHAR2",
+          value: artistId
+        }
+      ]
+    })
+  }
+
   static getIds = async (
     limit: number,
     offset: number
   ): Promise<any[]> => {
     return await postStatement({
-      statementText: "SELECT ID FROM ARTIST",
+      statementText: "SELECT ID FROM ARTIST ORDER BY ID",
       offset,
       limit
     });
@@ -29,14 +51,17 @@ export class ArtistAPI {
     return ids.map(id => id.id);
   };
 
-  static getByPlayTime = async (
-    offset: number = 0,
-    limit: number = 100,
-    rangeStart?: Date,
-    rangeEnd?: Date
-  ): Promise<any[]> => {
-    const start = dateToTimestamp(rangeStart ? rangeStart : new Date(0, 0, 1));
-    const end = dateToTimestamp(rangeEnd ? rangeEnd : new Date(9999, 0, 1));
+  static getByPlayTime = async (options?: getByPlayTimeOptions): Promise<any[]> => {
+    let start = dateToTimestamp(new Date(0, 0));
+    let end = dateToTimestamp(new Date(9999, 0));
+
+    if (options?.rangeStart) {
+      start = dateToTimestamp(options.rangeStart);
+    }
+    if (options?.rangeEnd) {
+      end = dateToTimestamp(options.rangeEnd);
+    }
+
 
     return await postStatement({
       statementText:
@@ -47,8 +72,8 @@ export class ArtistAPI {
         "WHERE PLAY.TS >= TO_TIMESTAMP(:start, 'YYYY-MM-DD HH24:MI:SS') AND PLAY.TS < TO_TIMESTAMP(:end, 'YYYY-MM-DD HH24:MI:SS') " +
         "GROUP BY ARTIST.ID, ARTIST.NAME, ARTIST.POPULARITY, ARTIST.FOLLOWERS " +
         "ORDER BY SUM(MS_PLAYED) DESC",
-      limit,
-      offset,
+      limit: options?.limit ? options.limit : 100,
+      offset: options?.offset ? options.offset : 0,
       binds: [
         {
           name: "start",
